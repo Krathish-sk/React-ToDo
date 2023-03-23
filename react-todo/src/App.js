31666d7dcec1bdc24142d1e37bda3e16314d8051
Krathish sk <krathishs.k123@gmail.com>
2023-03-23 12:11:11 +0530

    Heading for the website

--- react-todo/src/App.js
+++ react-todo/src/App.js
@@ -5,6 +5,9 @@ import TodoWrapper from "./Components/TodoWrapper";
 function App() {
   return (
     <div className="App">
+      <br />
+      <br />
+      <h2>Add Task and be Punctual</h2>
       <TodoWrapper />
     </div>
   );
