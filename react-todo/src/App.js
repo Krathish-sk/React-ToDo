a2f367c7b034cf87552e880420fee37b6a528eec
Krathish sk <krathishs.k123@gmail.com>
2023-03-23 20:35:35 +0530

    netlify error fix

--- react-todo/src/App.js
+++ react-todo/src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <br />
       <br />
-      <h2>Add Task and be Punctual</h2>
+      <h2 style={{ color: "#9E4784" }}>To - Do Manager</h2>
       <TodoWrapper />
     </div>
   );
